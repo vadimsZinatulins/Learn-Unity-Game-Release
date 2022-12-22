56bc10d253a84a187e3e72f6cffe52f1220db273
vadimszinatulins <vadimz2@hotmail.com>
2022-12-22 03:49:03 +0000

    Added player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 2.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * speed;
+
+        if(move.magnitude > speed)
+        {
+            move = move.normalized * speed;
+        }
+
+        transform.Translate(move);
     }
 }
